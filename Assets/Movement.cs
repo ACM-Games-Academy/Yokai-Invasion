1fd2a6fc02b4f245f739081f0bbf0ee75116efcf
ww3564 <ww3564@my.acm.ac.uk>
2025-09-29 15:44:41 +0100

    Creating movement
    I have created "Inputs" and "Movement" for future purposes so that the enemies can move around. (If you want it can be deleted without clashes)

---
+++ Assets/Movement.cs
@@ -0,0 +1,22 @@
+using System.Security.Cryptography;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] private Vector2 movementInput = Vector2.zero;
+    [SerializeField] float movement;
+
+    private bool moving = false;
+
+    private void Update()
+    {
+        Vector3 newMovementInput = new Vector3(movementInput.x, 0, movementInput.y);
+        transform.position += newMovementInput * Time.deltaTime * movement;
+    }
+
+    public void SyncMovementInput(InputAction.CallbackContext input) 
+    { 
+        movementInput = input.ReadValue<Vector2>();
+    }
+}
