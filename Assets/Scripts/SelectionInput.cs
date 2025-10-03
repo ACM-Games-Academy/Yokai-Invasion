ab9deddc7f6d146fdfa87633f9e8b43a3babe455
ACM-Sh4dow <km3391@my.acm.ac.uk>
2025-10-03 13:39:20 +0100

    Implemented Modify Select
    Now modifies selection logic based on whether Shift is held. When held allows adding or removing individuals from current selection, otherwise will reset selection when selecting

--- Assets/Scripts/SelectionInput.cs
+++ Assets/Scripts/SelectionInput.cs
@@ -1,8 +1,12 @@
+using UnityEditor.Build;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class SelectionInput : MonoBehaviour
 {
+
+    private bool isModifying = false;
+
     public void HandleSelectionInput(InputAction.CallbackContext context)
     {
         if (!context.started) return;
@@ -10,13 +14,43 @@ public class SelectionInput : MonoBehaviour
         if (Physics.Raycast(Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue()), out RaycastHit hitInfo) &&
             hitInfo.collider.TryGetComponent<SelectableUnit>(out SelectableUnit Unit))
         {
-            Debug.Log($"Clicked on {Unit.name}");
-            SelectionManager.Instance.Select(Unit);
+            if (isModifying && SelectionManager.Instance.IsSelected(Unit))
+            {
+                Debug.Log($"Deselecting {Unit.name}");
+                SelectionManager.Instance.Deselect(Unit);
+                return;
+            }
+            else if (isModifying && !SelectionManager.Instance.IsSelected(Unit))
+            {
+                Debug.Log($"Adding {Unit.name} to selection");
+                SelectionManager.Instance.Select(Unit);
+                return;
+            }
+            else
+            {
+                Debug.Log($"Clicked on {Unit.name}");
+                SelectionManager.Instance.ClearSelection();
+                SelectionManager.Instance.Select(Unit);
+            }
         }
         else
         {
             Debug.Log("Clicked on empty space, clearing selection");
             SelectionManager.Instance.ClearSelection();
+        }
+    }
+
+    public void ToggleModify(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            Debug.Log("Modify key pressed");
+            isModifying = true;
+        }
+        else if (context.canceled)
+        {
+            Debug.Log("Modify key released");
+            isModifying = false;
         }
     }
 }
